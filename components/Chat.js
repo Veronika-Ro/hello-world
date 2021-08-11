8e8d5fc290e54dcb28bf29b86af9d933de728eaf
Veronika-Ro <79661110+Veronika-Ro@users.noreply.github.com>
2021-08-11 13:32:38 +0100

    Chat UI updated

--- components/Chat.js
+++ components/Chat.js
@@ -1,30 +1,76 @@
 import React from 'react';
-import { StyleSheet, View, Text, TextInput, Alert, Button, ScrollView } from 'react-native';
+import { StyleSheet, View, Text, TextInput, Alert, Button, ScrollView, Platform, KeyboardAvoidingView } from 'react-native';
+import { GiftedChat, Bubble } from 'react-native-gifted-chat'
 
 
 export default class Chat extends React.Component {
-    render() {
-        let backgroundColor = this.props.route.params.backgroundColor;
+    constructor() {
+        super();
+        this.state = {
+            messages: [],
+        };
+    }
+
+
+    componentDidMount() {
         let name = this.props.route.params.name;
         this.props.navigation.setOptions({ title: name });
+        this.setState({
+            messages: [
+                {
+                    _id: 1,
+                    text: 'Hello developer',
+                    createdAt: new Date(),
+                    user: {
+                        _id: 2,
+                        name: 'React Native',
+                        avatar: 'https://placeimg.com/140/140/any',
+                    },
+                },
+                {
+                    _id: 2,
+                    text: `${name} entered the chat`,
+                    createdAt: new Date(),
+                    system: true,
+                },
+            ]
+        })
+    }
+
+    onSend(messages = []) {
+        this.setState(previousState => ({
+            messages: GiftedChat.append(previousState.messages, messages),
+        }))
+    }
+
+    renderBubble(props) {
+        return (
+            <Bubble
+                {...props}
+                wrapperStyle={{
+                    right: {
+                        backgroundColor: '#cdc8da'
+                    }
+                }}
+            />
+        )
+    }
+
+    render() {
+        let backgroundColor = this.props.route.params.backgroundColor;
 
         return (
-            <View style={[styles.backgroundColor(backgroundColor), styles.container]}>
-                <Text>Chat room is under construction ...</Text>
+            <View style={{ flex: 1, backgroundColor }}>
+                <GiftedChat
+                    renderBubble={this.renderBubble.bind(this)}
+                    messages={this.state.messages}
+                    onSend={messages => this.onSend(messages)}
+                    user={{
+                        _id: 1,
+                    }}
+                />
+                {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
             </View>
         )
     }
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: '100%'
-    },
-    // Brings over selected background color selected in home screen
-    backgroundColor: (backgroundColor) => ({
-        backgroundColor: backgroundColor,
-    }),
-});
